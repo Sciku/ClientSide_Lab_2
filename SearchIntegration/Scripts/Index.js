bc3e7c1d9324f344a882647bc6ddcb2f2154e975
Scien <timothy.brostrom@gmail.com>
2016-01-08 14:37:17 +0100

    Gjort en GetAll funktion samt funkar sökfunktionen

--- SearchIntegration/Scripts/Index.js
+++ SearchIntegration/Scripts/Index.js
@@ -1,1 +1,32 @@
-﻿
+﻿$(document).ready(function () {
+    searchFunction();
+    $('#search').keydown(function ()
+    {
+        searchFunction();
+    });
+
+});
+    function searchFunction() {
+            var searchfield = $('#search').val();
+            var myExpression = new RegExp(searchfield, 'i');
+            $.getJSON('Menuitems.json', function (data) {
+                var output = "<ul class='searchresults'>";
+                $.each(data, function (key, val) {
+
+                    if ((val.name.search(myExpression) !== -1) ||
+                        (val.about.search(myExpression) !== -1)) {
+                        output += "<li>";
+                        output += "<p>";
+                        output += val.name;
+                        output += val.about;
+                        output += "</p>";
+                        output += "<img src='Images/" + val.image
+                        output += "'alt='" + val.name;
+                        output += "'/>";
+                        output += "</li>";
+                    }
+                });
+                output += "</ul>";
+                $('#update').html(output);
+            });
+    }
