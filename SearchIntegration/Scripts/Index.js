e0ab823213efbc81b030f3c3889bda2d61cf2a39
Scien <timothy.brostrom@gmail.com>
2016-01-10 20:21:12 +0100

    Fixat med infon i JSON-filen samt fixat så att sorteringen fungerar

--- SearchIntegration/Scripts/Index.js
+++ SearchIntegration/Scripts/Index.js
@@ -1,32 +1,41 @@
 ﻿$(document).ready(function () {
-    searchFunction();
-    $('#search').keydown(function ()
-    {
-        searchFunction();
+    searchFunction();  
+
+    $(".box").change(function () {
+        searchFunction();  //Triggas när man ändrar i en checkbox
     });
 
-});
-    function searchFunction() {
-            var searchfield = $('#search').val();
-            var myExpression = new RegExp(searchfield, 'i');
-            $.getJSON('Menuitems.json', function (data) {
-                var output = "<ul class='searchresults'>";
-                $.each(data, function (key, val) {
+    $('#search').keydown(function () {
+        searchFunction();    
+    });
+   
+}) ;
 
-                    if ((val.name.search(myExpression) !== -1) ||
-                        (val.about.search(myExpression) !== -1)) {
-                        output += "<li>";
-                        output += "<p>";
-                        output += val.name;
-                        output += val.about;
-                        output += "</p>";
-                        output += "<img src='Images/" + val.image
-                        output += "'alt='" + val.name;
-                        output += "'/>";
-                        output += "</li>";
-                    }
-                });
-                output += "</ul>";
-                $('#update').html(output);
-            });
-    }
+
+function searchFunction() {
+    var output = "<ul class='searchresults'>";
+    var searchfield = $('#search').val();
+    var myExpression = new RegExp(searchfield, 'i');
+    $.getJSON('Menuitems.json', function (data) {
+        $.each(data, function (key, val) {
+
+            if ((val.name.search(myExpression) !== -1) ||
+                (val.about.search(myExpression) !== -1)) {
+                if ($("#" + val.category).is (":checked")) {   //Eftersom jag har samma namn på på mina checkbox-id som category i JSON filen kollar jag om någon av dessa checkboxarna är iklickad
+
+                    output += "<li>";
+                    output += "<p>";
+                    output += val.name;
+                    output += val.about;
+                    output += "</p>";
+                    output += "<img src='Images/" + val.image
+                    output += "'alt='" + val.name;
+                    output += "'/>";
+                    output += "</li>";
+                }
+            }
+        });
+        output += "</ul>";
+        $('#update').html(output);
+    });
+}
